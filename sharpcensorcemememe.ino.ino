0ec93fbd936e798f5b76324541ad87ff35607b35
Thomas Li <77999105+ThomasDLi@users.noreply.github.com>
2021-01-27 20:06:59 -0800

    Updated code comments
    updated the comments in the code so you can navigate the code slightly better

--- sharpcensorcemememe.ino.ino
+++ sharpcensorcemememe.ino.ino
@@ -13,9 +13,9 @@
 //                                            20150 for GP2Y0A02Y
 //                                            (working distance range according to the datasheets)
 
-SharpIR SharpIR1(ir, model);
-SharpIR SharpIR2(irb, model);
-SharpIR SharpIR3(irq, model);
+SharpIR SharpIR1(ir, model);  // middle sensor
+SharpIR SharpIR2(irb, model); // lower sensor
+SharpIR SharpIR3(irq, model); // upper sensor
 Button button1(12);
 void setup() {
   Serial.begin(9600);
@@ -30,7 +30,7 @@ void loop() {
   int dis1=SharpIR2.distance();
   int dis2=SharpIR3.distance();
 
-
+// start of debugging code. this was just code that I used while debugging the program. You can remove it if you want.
   Serial.print("Mean distance 1: ");
   Serial.println(dis); 
 
@@ -39,8 +39,10 @@ void loop() {
 
   Serial.print("Mean distance 3: ");
   Serial.println(dis2); 
+  // end of debugging code
   int x=190;
   int ex=190; 
+  // start of 2nd debugging code block
   Serial.println("lower back angle");
   float y=dis1 - dis;
   float ans=atan(y/x);
@@ -49,7 +51,9 @@ void loop() {
   float ey=dis2 - dis;
   float anss=atan(ey/ex);
   Serial.println(anss * 100);
-  // change the ">15" and "< -3" to adjust the threshhold
+  // end of 2nd debugging code block
+  
+  // change the ">15" and "< -3" to adjust the threshhold (how far you can lean without it beeping)
   if ((anss * 100) > 15 or (ans * 100) < -3){
     Serial.println("bad posture");
       tone(8, 440, 1000);
